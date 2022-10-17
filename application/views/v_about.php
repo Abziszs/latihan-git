01e28e6c347bc8fc87a75979812109fb4fbe1835
Abziszs <94218991+Abziszs@users.noreply.github.com>
2022-10-17 16:28:19 +0700

    Add files via upload

---
+++ application/views/v_about.php
@@ -0,0 +1,40 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama Lengkap :</h4>
+    <ul type="disc">
+        <li>Nama Lengkap: Umar Abdul Azis</li>
+ </ul>
+ <br>
+ <h4>Tempat Lahir, Tanggal lahir:</h4>
+ <ul type="none">
+    <li>Jakarta, 20/03/2001</li>
+</ul>
+ <h4>Alamat:</h4>
+ <ul type="none">
+    <li> Kebon Baru Gang III Blok.X No.19</li>
+ </ul>
+ <h4>Jenis Kelamin:</h4>
+ <ul type="none">
+    <li> Laki-laki</li>
+ </ul>
+ <h4>Agama:</h4>
+ <ul type="none">
+    <li> Islam</li>
+ </ul>
+ <h4>Status:</h4>
+ <ul type="none">
+    <li> Belum Kawin</li>
+ </ul>
+ <h4>Pekerjaan:</h4>
+ <ul type="none">
+    <li> Pelajar/Mahasiswa</li>
+ </ul>
+
+ <h4>Olah Raga Favorit:</h4>
+ <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Running</li>
+        <li>Renang</li>
+        <li>Volly</li>
+ </ul>
+</section>
